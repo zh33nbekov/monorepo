3d2d7b887181f0d12b6b724d20f294e82330b5f9
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-09 21:20:02 +0600

    fixed

--- services/admin/src/app/page.tsx
+++ services/admin/src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@packages/shared'
 const Home = () => {
-	return <div>ClientPage</div>
+	return (
+		<div>
+			<Button type='link' href='http://localhost:3000'>
+				Go to home page
+			</Button>
+			<h1>AdminPage</h1>
+		</div>
+	)
 }
 
 export default Home
--- services/client/src/app/page.tsx
+++ services/client/src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@packages/shared'
+
 const Home = () => {
-	return <div>AdminPage</div>
+	return (
+		<div>
+			<Button type='link' href='http://localhost:3001'>
+				Go to admin page
+			</Button>
+			<h1>HomePage</h1>
+		</div>
+	)
 }
 
 export default Home
