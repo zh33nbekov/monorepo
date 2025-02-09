d337d3da2d7a862b7dc2d8313b1277978a1bd848
zh33nbekov <rai.zheenbekov@gmail.com>
2025-02-09 21:59:15 +0600

    dev/fixed

--- services/admin/src/app/page.tsx
+++ services/admin/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@packages/shared'
 const Home = () => {
 	return (
 		<div>
-			<Button type='link' href='http://localhost:3000'>
+			<Button type='link' href='https://monorepo-client-smoky.vercel.app/'>
 				Go to home page
 			</Button>
 			<h1>AdminPage</h1>
--- services/client/src/app/page.tsx
+++ services/client/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@packages/shared'
 const Home = () => {
 	return (
 		<div>
-			<Button type='link' href='http://localhost:3001'>
+			<Button type='link' href='https://monorepo-admin-beta.vercel.app/'>
 				Go to admin page
 			</Button>
 			<h1>HomePage</h1>
